49f41c4ac4a619272f73820b73e862183f94826d
Salima <salima.almeras@laposte.net>
2021-08-12 13:07:47 +0100

    Ajouter

--- fronted/js/product.js
+++ fronted/js/product.js
@@ -20,10 +20,18 @@
 //let params = new URL(document.location).searchParams;
 //let id = params.get("Produit.id");
 //console.log(Produit.id);
+//let url = new URL(url_str);
+//let search_params = url.searchParams;
+
+// true
+//if (search_params.has('id')) {
+// "100"
+//  console.log(search_params.get('id'))
+//}
 
 let params = new URL(document.location).searchParams;
-let id = params.get("id");
-fetch(`http://localhost:3000/api/teddies/+${id}`)
+let Produit_id = params.get("id");
+fetch("http://localhost:3000/api/teddies/+${Produit_id}")
     .then(function(response) {
         return response.json();
     })
